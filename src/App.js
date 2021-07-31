697397782dbd34526b377df58ba94ea0339e863b
Harrison <harrisonlara97@gmail.com>
2021-07-31 12:05:29 -0500

    feat: generate list of cards with contacts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import React from 'react'
+import { ContactsListLanding } from './components'
+import { ContactsProvider } from './context'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <div>Visa Interview</div>
-      </header>
-    </div>
-  );
+    <main className="app">
+      <ContactsProvider>
+        <ContactsListLanding />
+      </ContactsProvider>
+    </main>
+  )
 }
 
-export default App;
+export default App
